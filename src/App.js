8d96c7d1f6447f4597c2bbafb1398c94a7b32045
ShadowWalker98 <44528434+ShadowWalker98@users.noreply.github.com>
2020-06-30 17:03:02 +0530

    added hooks

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+  const [users, setUsers] = useState([]);
+  console.log("Hi");
+  const getUsers = async() => {
+    let res = await fetch("https://jsonplaceholder.typicode.com/users");
+    let users = await res.json();
+    setUsers(users);
+  }
+
+  useEffect(() => {
+    getUsers();
+
+  }, []);
+
+  return(
+    <div>
+      <ul>
+        {users.map(user => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
     </div>
-  );
+  )
 }
 
 export default App;
